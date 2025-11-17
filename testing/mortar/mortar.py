6d138bac100eefd707d04d15012fcf5122238faf
Lazlo <lazlopaul17@gmail.com>
2025-11-17 09:05:04 -0800

    running on mortar

--- testing/mortar/mortar.py
+++ testing/mortar/mortar.py
@@ -2,7 +2,7 @@
 NOTE: Routinely hitting max iterations even with low required similarity thresholds. Would be good to discover why. 
 The amount of max iterations is related to the shortest length between a distinguishing characteristic and another class. In these graphs that shortest distance could possibly be larger than 10 (though it is surprising that it seems to be higher than 10 for all of them)
 """
-
+import csv
 import os 
 import sys 
 sys.path.append('../../')
@@ -32,14 +32,14 @@ if __name__ == "__main__":
     g_lens = []
     cg_lens = []
     file_names = []
-    threshold = 0.5
+    threshold = 0.7
     i = 0 
     # TODO: see what it looks like removing labels and stuff like that. 
     for file_name, g in get_mortar_graphs(directory_path):
         gs.append(g)
         g_lens.append(len(g))
         file_names.append(file_name)
-        cg, mg = run_algo(g, iterations=5,similarity_threshold=threshold)
+        cg, mg = run_algo(g, iterations=10, similarity_threshold=threshold)
         # removing extraneous classes
         for s,p,o in cg:
             if (p == A) & (str(HPFS) in str(o)):
@@ -49,8 +49,8 @@ if __name__ == "__main__":
         print("compressed to ", len(cg)/len(g)*100, "% of its original size")
         # cg.print()
         cg_lens.append(len(cg))
-        if i >= 15:
-            break
+        # if i >= 15:
+        #     break
         i += 1
     # size of original graph vs size of compressed graph 
     plt.plot(g_lens, cg_lens, 'o')
@@ -58,9 +58,21 @@ if __name__ == "__main__":
     plt.ylabel("Compressed Graph Size")
     plt.title(f"Mortar Building Compression By Graph Size (Threshold: {threshold})")
     plt.savefig("mortar_graph_sizes.png")
-    plt.show()
+    plt.clf()
     plt.plot(g_lens, [(cg_len/g_len) for cg_len, g_len in zip(cg_lens, g_lens)], 'o')
     plt.xlabel("Original Graph Size")
     plt.ylabel("Compression Ratio")
     plt.title(f"Mortar Building Compression Ratio By Graph Size (Threshold: {threshold})")
     plt.savefig("compression_ratios.png")
+
+    with open('lengths.csv', 'w', newline='') as csvfile:
+        # Create a CSV writer object
+        writer = csv.writer(csvfile)
+
+        # Optionally, write a header row
+        writer.writerow(["graph_length", "bschema_length"])
+
+        # Use zip to combine the elements of the two lists into pairs
+        # Then, write each pair as a row in the CSV
+        for item1, item2 in zip(g_lens, cg_lens):
+            writer.writerow([item1, item2])
